5201c97bd2f11b92f49464f85d6b35a349b59bf3
Abdul Hadi Bharara <32545366+bharara@users.noreply.github.com>
2019-04-24 19:39:09 +0500

    Create 4 - Largest palindrome product.py

---
+++ 4 - Largest palindrome product.py
@@ -0,0 +1,18 @@
+# Palindrome
+def palin(num):
+    return str(num) == str(num)[::-1]        
+
+a = 100
+large = 0
+while a < 1000:
+    b = a
+    while b < 1000:
+        if a*b > large and palin(a*b):
+            large = a*b
+            break
+        b += 1
+    if a*b > large and palin(a*b):
+        break
+    a += 1
+    
+print(large)
