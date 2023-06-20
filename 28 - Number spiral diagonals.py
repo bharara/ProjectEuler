b4c1ec28ff67900b67d2bdf4098416d0086728e7
Abdul hadi <ahadi.bese17seecs@seecs.edu.pk>
2023-06-20 11:23:42 +0500

    updated problem 28 solution

--- 28 - Number spiral diagonals.py
+++ 28 - Number spiral diagonals.py
@@ -1,10 +1,12 @@
 n = 1001
 
+s = 1
+k = 1
 
-s = [1]
 for mul in range(2, n, 2):
-	base  = s[len(s)-1]
+	base  = k
 	for i in range(1,5):
-		s.append(i*mul + base)
+		k = i*mul + base
+		s += k
 
-print(sum(s))
+print(s)
